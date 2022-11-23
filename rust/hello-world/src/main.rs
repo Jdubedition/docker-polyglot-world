cdd4c1dcc85f70adc45b873f7ae4d2d84ef01c57
Justin Wesley <justin.t.wesley@gmail.com>
2022-11-22 22:03:39 -0700

    add rust

---
+++ rust/hello-world/src/main.rs
@@ -0,0 +1,43 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::header::{HeaderValue, CONTENT_TYPE};
+use gethostname::gethostname;
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!("INFO: received request");
+    let mut response = Response::new(format!("{{\"hello\":\"World\", \"from\":{:?}}}", gethostname()).into());
+    response.headers_mut().insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    Ok(response)
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    // Run this server for... forever!
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
+}
---
+++ rust/rust.Dockerfile
@@ -0,0 +1,10 @@
+FROM rust:1.65 as builder
+WORKDIR /usr/src/hello-world
+COPY hello-world .
+RUN cargo install --path .
+
+FROM debian:buster-slim
+RUN apt-get update && apt-get install -y && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /usr/local/cargo/bin/hello-world /usr/local/bin/hello-world
+EXPOSE 3000
+CMD ["hello-world"]
